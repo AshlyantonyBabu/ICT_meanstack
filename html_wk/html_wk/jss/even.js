0236b37c08217b93cffcac4b4c260572d068ae95
AshlyantonyBabu <elsyantonybabu@gmail.com>
2019-07-25 08:28:15 +0530

    hw

--- html_wk/html_wk/jss/even.js
+++ html_wk/html_wk/jss/even.js
@@ -1,9 +1,20 @@
-var n=98;
-if(n%2==0)
+function funeven()
 {
-    console.log("the number is even");
+var n=document.forms["frm1"]["name"].value;
+
+   //var n1 = parseInt(document.getElementById("name").Value);
+   var n1=(n%2);
+if(n1==0)
+{
+   
+  document.getElementById("lb1").innerHTML= "the number is even";
+  console.log("even");
+  //console.log(n1);
 }
 else
 {
-    console.log("number is odd");
+    //console.log("odd");
+    console.log(n);
+    document.getElementById("lb1").innerHTML= "the number is odd";
+}
 }
