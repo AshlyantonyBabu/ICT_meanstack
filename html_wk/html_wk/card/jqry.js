253291d3c1e64f3eabad9c8354cafb182ce84625
AshlyantonyBabu <elsyantonybabu@gmail.com>
2019-07-31 16:37:41 +0530

    dfff

--- html_wk/html_wk/card/jqry.js
+++ html_wk/html_wk/card/jqry.js
@@ -1,42 +1,85 @@
 
 
-function ajaxTest() {
+//function ajaxTest() {
 $(document).ready(function(){
     console.log("aaaa");
-    $("#butn").click(function(){
+    
+    
+   // $("#butn").click(function(){
         console.log("aaaa");
     $.ajax({
         type:"GET",
         url:"libr.json",
         //dataType: "text",
+       
+
+        
         success:function(data){
             var output=" ";
+            var showChar = 100;
+            var ellipsestext = "...";
+             var moretext = "more";
+            var lesstext = "less";
+            var c=" ";
             alert("load");
             for(var i in data){
-                output+="<div class='card'style='width: 18rem;'>"
-                output+="<img class='card-img-top' src='"+data[i].pic +"'>"
+                var content =  data[i].discription;
+                if(content.length > showChar) {
+                    c = content.substr(0, showChar);
+                    var h = content.substr(showChar-1, content.length - showChar);
+                    console.log(c);
+                    var arr = c ;
+                    console.log(h);
+              
+                
+                }
+
+                output+=" <div class='col col-12 col-sm-3 '>"
+                output+="<div class='card'style='width: 18rem; '>"
+                output+="<img class='card-img-top' src='"+data[i].pic +" 'height='300px' width='50px' >"
                 output+="<div class='card-body'>"
-                output+=  "<h5 class='card-title'>"+ data[i].title+"</h5>"
+                output+=  "<b><center><h class='card-title' style='font-style: italic ;font: size 5px;'>"+ data[i].title+"</b></center></h>"
             
-                output+=  "<p class='card-text'>"+ data[i].discription+"</p>"
+                output+=  "<p class='card-text'style='text-align:justify ;font-style: italic ;' id='p1'>"+ arr +" "+ ellipsestext+"<a href='' id='redmr'>Readmore</a>" + " </p>"
+               
+              
                 output+= "</div>"
                 output+="<ul class='list-group list-group-flush'>"
                  output+= "<li class='list-group-item'>"+ data[i].author+"</li>"
                  output+=  "<li class='list-group-item'>"+ data[i].category+"</li>"
-                 output+= "<li class='list-group-item'>"+ data[i].price+"</li>"
+                 output+= "<li class='list-group-item'> <b>Price :  </b>"+ data[i].price+"</li>"
                  output+= "</ul>"
-                output+="<div class='card-body'>"
-                output+=  "<a href='' class='card-link'>Card link</a>"
-                output+= "<a href='' class='card-link'>Another link</a>"
-                output+= "</div>"
+               // output+="<div class='card-body'>"
+                //output+=  "<a href='' class='card-link'>Card link</a>"
+               // output+= "<a href='' class='card-link'>Another link</a>"
+               // output+= "</div>"
+           output+=" </div>"
            output+=" </div>"
     
     
             }
             $("#dd").html(output);
+        
         }
 
     })
-})
+    $("#redmr").click(function(){
+        output+=" <div class='col col-12 col-sm-3 '>"
+        output+="<div class='card'style='width: 18rem; '>"
+        output+="<img class='card-img-top' src='"+data[i].pic +" 'height='300px' width='50px' >"
+        output+="<div class='card-body'>"
+        output+=  "<b><center><h class='card-title' style='font-style: italic ;font: size 5px;'>"+ data[i].title+"</b></center></h>"
+        $("#p1").hide();
+        output+=  "<p class='card-text'style='text-align:justify ;font-style: italic ;'>"+ arr +h+ " </p>"
+        output+= "</div>"
+        output+="<ul class='list-group list-group-flush'>"
+         output+= "<li class='list-group-item'>"+ data[i].author+"</li>"
+         output+=  "<li class='list-group-item'>"+ data[i].category+"</li>"
+         output+= "<li class='list-group-item'> <b>Price :  </b>"+ data[i].price+"</li>"
+         output+= "</ul>"
+
     })
-}
+    
+})
+  //  })
+//}
